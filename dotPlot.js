6d17bde3aa46badaebcf7ac722c1a5fef368b52e
josephnacci <joseph@naccidigital.com>
2019-10-27 17:47:06 -0400

    wrap labels

--- dotPlot.js
+++ dotPlot.js
@@ -303,7 +303,8 @@ var dotPlot = (function dotPlot(url, selector, params){
 			    else{
 				return 'black'
 				    }
-			});
+			})
+		    .call(wrap, 200);
 		
 
 
@@ -339,6 +340,35 @@ var dotPlot = (function dotPlot(url, selector, params){
                             }
                         })
                     .text(title);
+
+
+
+
+		function wrap(text, width) {
+		    text.each(function() {
+			    var text = d3.select(this),
+				words = text.text().split(/\s+/).reverse(),
+				word,
+				line = [],
+				lineNumber = 0,
+				lineHeight = 1.1, // ems
+				y = text.attr("y"),
+				dy = parseFloat(text.attr("dy")),
+				tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em")
+				while (word = words.pop()) {
+				    line.push(word)
+				    tspan.text(line.join(" "))
+				    if (tspan.node().getComputedTextLength() > width) {
+					line.pop()
+					tspan.text(line.join(" "))
+					line = [word]
+					tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", `${++lineNumber * lineHeight + dy}em`).text(word)
+				    }
+				}
+			})
+			}
+
+
 
 		
 	    })
